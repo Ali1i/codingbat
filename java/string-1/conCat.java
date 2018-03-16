53920147f7fe31da7eb66e83bfb8bde688e89602
Ali1i <36134682+Ali1i@users.noreply.github.com>
2018-03-16 19:14:56 -0400

    Update conCat.java

--- java/string-1/conCat.java
+++ java/string-1/conCat.java
@@ -9,5 +9,5 @@ public String conCat(String a, String b) {
     if(a.charAt(a.length() - 1) == b.charAt(0))
         return a + b.substring(1);
                     
-    return a + b;
+    return a +b;
 }
